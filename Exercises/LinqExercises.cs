428eae639fa1e9962c59cebf768a82a49a4883c7
Artem Diachuk <s33796@pjwstk.edu.pl>
2026-03-27 12:54:56 +0100

    Complete Task 01 and Task 02

--- Exercises/LinqExercises.cs
+++ Exercises/LinqExercises.cs
@@ -16,7 +16,9 @@ public sealed class LinqExercises
     /// </summary>
     public IEnumerable<string> Task01_StudentsFromWarsaw()
     {
-        throw NotImplemented(nameof(Task01_StudentsFromWarsaw));
+        return UniversityData.Students
+            .Where(s => s.City == "Warsaw")
+            .Select(s => $"{s.IndexNumber} {s.FirstName} {s.LastName} ({s.City})");
     }
 
     /// <summary>
@@ -30,7 +32,8 @@ public sealed class LinqExercises
     /// </summary>
     public IEnumerable<string> Task02_StudentEmailAddresses()
     {
-        throw NotImplemented(nameof(Task02_StudentEmailAddresses));
+        return UniversityData.Students
+            .Select(s => s.Email);
     }
 
     /// <summary>
